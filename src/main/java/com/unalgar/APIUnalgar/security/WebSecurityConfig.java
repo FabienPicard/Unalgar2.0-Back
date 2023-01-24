668dc46db0e781c2c449387eae42126936afd2e7
FabienPicard <fabien.picard69@gmail.com>
2023-01-24 11:09:12 +0100

    fix bug: save score button

--- src/main/java/com/unalgar/APIUnalgar/security/WebSecurityConfig.java
+++ src/main/java/com/unalgar/APIUnalgar/security/WebSecurityConfig.java
@@ -1,7 +1,10 @@
 package com.unalgar.APIUnalgar.security;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -22,6 +25,11 @@ import java.util.Arrays;
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Value("${frontend.url}")
     private String frontendUrl;
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
